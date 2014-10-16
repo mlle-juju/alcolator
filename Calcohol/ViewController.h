ba37646407ba2b4d0307d7047a50dba666fdbb63
Julicia James <d.james128@gmail.com>
2014-10-16 06:28:13 -0400

    Initial Commit

---
+++ Calcohol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcohol
+//
+//  Created by Julicia on 10/16/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calcohol/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calcohol
+//
+//  Created by Julicia on 10/16/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
